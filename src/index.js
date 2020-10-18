d8639e1a425a37b49a5b5882ab354d7bc6d064a2
gabihtoledo0 <gabriela.prado@youse.com.br>
2020-10-18 19:23:57 -0300

    added material-ui library

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { render } from "react-dom";
 
 export function App() {
   return (
-    <p className="text-purple-500">
-      Oii
-    </p>
+    <>
+      <p className="text-purple-500">
+        Oii
+      </p>
+  </>
   );
 }
 
