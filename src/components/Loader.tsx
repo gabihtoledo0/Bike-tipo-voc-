51e3e36e211a3ca385cd153dbb027d4d7d925f7d
gabihtoledo0 <gabriela.prado@youse.com.br>
2020-12-29 18:41:35 -0300

    added loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { useTheme } from "@material-ui/core/styles"
+import Text from "../components/Text"
+import { CircularProgress } from "@material-ui/core"
+
+interface LoaderProps {
+  data: String
+}
+
+const Loader = ({ data }: LoaderProps) => {
+  const theme = useTheme()
+
+  return (
+    <div
+      className="flex justify-center items-center"
+      style={{ height: "100vh" }}
+    >
+      <CircularProgress style={{ color: theme.colors.color.primaryInverse }} />
+      <div className="pt-4">
+        <Text align="center" color={theme.colors.color.primaryInverse}>
+          {data}
+        </Text>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
